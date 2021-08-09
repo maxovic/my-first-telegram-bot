7d758d55649998c805f6f336d76ee677e60107de
maxovic <maxat.tileuov@gmail.com>
2021-08-09 10:29:38 +0600

    delete ignored files

--- target/classes/liquibase/vlife/user/user.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset maxat:1.0
-CREATE TABLE IF NOT EXISTS vlife.user (
-    id BIGSERIAL NOT NULL,
-    telegram_user_id BIGINT NOT NULL,
-    username VARCHAR,
-    is_registered BOOLEAN DEFAULT FALSE
-)
--- target/classes/liquibase/vlife/user_preference/user_preference.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset maxat:1.0 runOnChange:true
-CREATE TABLE IF NOT EXISTS vlife.user_preference (
-    id BIGSERIAL NOT NULL,
-    telegram_user_id BIGINT NOT NULL,
-    breed VARCHAR NOT NULL,
-    is_liked BOOLEAN NOT NULL
-)
